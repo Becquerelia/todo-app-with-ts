fdcdc583d361f7ce7467cbb0aaa84fbf47574ce1
Eva GC <evarteca@gmail.com>
2022-07-06 10:53:53 +0200

    feat addTask function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { FC, useState, ChangeEvent } from "react";
 import "./App.css";
+import {Task} from "./interfaces";
 
 const App: FC = () => {
 
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
-  const [todo, setTodoList] = useState<[]>([]);
+  const [todoList, setTodoList] = useState<Task[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task"){
@@ -15,7 +16,13 @@ const App: FC = () => {
     } 
   }
   
-  
+  const addTask = ():void => {
+    const newTask = {taskName: task, deadline: deadline};
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
+  }
+
   return (
     <div className="App">
       <h1>ToDo App</h1>
@@ -25,7 +32,7 @@ const App: FC = () => {
           <input type="number" placeholder="Deadline (in Days)..." name="deadline" value={deadline} onChange={handleChange} />
         </div>
 
-        <button >Add Task</button>
+        <button onClick={addTask} >Add Task</button>
       </div>
       <div className="todoList"></div>
     </div>
